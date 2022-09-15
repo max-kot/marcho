120e6c71df9554dc123d0ce4b6cfc975f76bf1fd
sim-max <max.kotlarenko@gmail.com>
2022-09-15 17:19:15 +0300

    added adaptive for header

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,19 @@
-console.log('Hola!');
-console.log('What does the fox say?');
+/*---Menu---*/
+const menuBtn = document.querySelector('.menu-btn');
+const menuList = document.querySelector('.menu__list');
+const body = document.querySelector('body');
+const menuLinks = document.querySelectorAll('.menu a')
+
+menuBtn.addEventListener('click', function () {
+	body.classList.toggle('no-scroll');
+	menuList.classList.toggle('active');
+	menuBtn.classList.toggle('active');
+
+})
+menuLinks.forEach(function (link) {
+	link.addEventListener('click', function () {
+		menuList.classList.remove('active');
+		body.classList.remove('no-scroll');
+		menuBtn.classList.remove('active');
+	})
+})
